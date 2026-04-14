0f7d703907bb74fcf87070a71d26370fed3679f4
SumanKumari0103 <sinhasuman0305@gmail.com>
2026-04-14 15:09:08 +0530

    Add Strings program

---
+++ Loops/Patterns/Functions&Methods/Arrays/Strings/Strings7.java
@@ -0,0 +1,12 @@
+
+public class Strings7 {
+    //String To Integer
+    public static void main(String args[]) {
+       String str = "123";
+       int number = Integer.parseInt(str);
+       System.out.println(number);
+      
+      
+   }
+
+}
