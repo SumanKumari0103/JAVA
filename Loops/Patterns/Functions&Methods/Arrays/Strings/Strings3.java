e196fa8427ee1ffa78324cc0a883c1a7f22cc6a9
SumanKumari0103 <sinhasuman0305@gmail.com>
2026-04-14 14:28:32 +0530

    Add Strings program

---
+++ Loops/Patterns/Functions&Methods/Arrays/Strings/Strings3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Strings3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter you first name:");
+        String first_name=sc.nextLine();
+        System.out.println("Enter your second name:");
+        String second_name=sc.nextLine();
+        String full_name=first_name +" "+second_name;
+        System.out.println("The length of string is:"+full_name.length());
+    }
+}
