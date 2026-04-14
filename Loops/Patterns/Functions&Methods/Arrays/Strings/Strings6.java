458a01eccc2ffb1a4b605d3734300bbb3f4c75be
SumanKumari0103 <sinhasuman0305@gmail.com>
2026-04-14 15:06:36 +0530

    Add Strings program

---
+++ Loops/Patterns/Functions&Methods/Arrays/Strings/Strings6.java
@@ -0,0 +1,7 @@
+public class Strings6 {
+    public static void main(String[] args) {
+        String s1="Ankit Sakshi";
+        String name=s1.substring(0,6);
+        System.out.println(name);
+    }
+}
