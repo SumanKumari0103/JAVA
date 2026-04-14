a7aeb766f0ee7ae98bd82e92d21db512f1d07a5a
SumanKumari0103 <sinhasuman0305@gmail.com>
2026-04-14 10:34:28 +0530

    Add Arrays program

--- Loops/Patterns/Functions&Methods/Arrays/Arrays7.java
+++ Loops/Patterns/Functions&Methods/Arrays/Arrays7.java
@@ -1,3 +1,23 @@
+import java.util.*;
 public class Arrays7 {
-    
+    public static void main(String[] args) {
+        //LARGEST ELEMENT IN AN ARRAY
+         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array :");
+        int size=sc.nextInt();
+
+        int arr[]=new int[size];
+        System.out.println("Enter the numbers in an array:");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int largest=arr[0];
+        for(int i=0;i<size;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        System.out.println("Largest Element is :"+largest);
+    }
 }
--- Loops/Patterns/Functions&Methods/Arrays/Arrays9.java
+++ Loops/Patterns/Functions&Methods/Arrays/Arrays9.java
@@ -1,6 +1,28 @@
 import java.util.*;
 public class Arrays9 {
     public static void main(String[] args) {
-        
+        //SECOND LARGEST IN AN ARRAY
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+
+        int arr[]=new int[size];
+        System.out.println("Enter the numbers");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int largest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+
+        for(int i=0;i<size;i++){
+            if(arr[i]>largest){
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>secondLargest && arr[i]!=largest){
+                secondLargest=arr[i];
+            }
+        }
+        System.out.println("Second Largest is :"+secondLargest);
     }
 }
