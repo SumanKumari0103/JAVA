fd8cb8d4208391bbebc4b297581d4b074c1d672c
SumanKumari0103 <sinhasuman0305@gmail.com>
2026-04-15 16:09:17 +0530

    Add StringsBuilder program

---
+++ Loops/Patterns/Functions&Methods/Arrays/Strings/StringBuilder/StringBuilder5.java
@@ -0,0 +1,14 @@
+
+
+public class StringBuilder5 {
+public static void main(String[] args) {
+    //Append
+    StringBuilder sb= new StringBuilder("h");
+    System.out.println(sb);
+    sb.append("e");
+    sb.append("l");
+    sb.append("l");
+    sb.append("o");
+    System.out.println(sb);
+}
+}
