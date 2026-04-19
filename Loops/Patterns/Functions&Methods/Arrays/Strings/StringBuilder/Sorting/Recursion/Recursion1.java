f0dc78f29f6b935c43d718a236a076c7c64e9434
SumanKumari0103 <sinhasuman0305@gmail.com>
2026-04-19 08:55:19 +0530

    Add Recursions program

---
+++ Loops/Patterns/Functions&Methods/Arrays/Strings/StringBuilder/Sorting/Recursion/Recursion1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Recursion1 {
+    public static void printNum(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        printNum(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        printNum(n);
+    }
+}
